db64da262344b11c6b3f735c7e9e9061eb5fe067
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-29 12:27:51 -0300

    refactored

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="display">
-    
-  </div>
+  <div class="display">{{ value }}</div>
 </template>
 
 <script>
 export default {
-  name: "Display",
+  name: 'Display',
+  props: {
+    value: {
+      type: String,
+      default: '0',
+    },
+  },
 };
 </script>
 
 <style lang="sass" scoped>
-  
+  .display
+    grid-column: span 4
+    display: flex
+    justify-content: flex-end
+    align-items: center
+    padding: 20px
+    font-size: 2.2em
 </style>
