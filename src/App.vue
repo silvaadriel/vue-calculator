60212a0f82acb9cc37fad515d8aa1857255fece5
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-29 15:05:52 -0300

    added media query

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,24 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-  h1
-    color: #fff7
-    font-size: 3em
-    margin-right: 80px
-
   #app
     display: flex
     justify-content: center
     align-items: center
     height: 100vh
     background: linear-gradient(120deg, #3FB37F, #33475B )
+
+  h1
+    color: #fff7
+    font-size: 3em
+    margin-right: 80px
+
+  @media (max-width: 720px)
+    #app
+      flex-direction: column
+
+    h1
+      margin: 0 0 40px 0
+      font-size: 2.5em
+
 </style>
