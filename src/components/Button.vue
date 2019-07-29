4f65bc301ca136dfaa8a443e0b81a7a0a84d6f4d
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-28 23:55:38 -0300

    added style

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,14 +1,36 @@
 <template>
-  <button class="button"></button>
+  <button class="button">{{ label }}</button>
 </template>
 
 <script>
 export default {
   name: "Button",
+  props: {
+    label: {
+      type: String
+    }
+  }
 };
 </script>
 
 <style lang="sass" scoped>
-  
+  .button
+    border: none
+    outline: none
+    background: white
+    font-size: 1.4em
+    color: black
+    font-weight: bold
+    margin: 1px
+
+    &:active
+      background: #fff5
+
+    &.operation
+      font-weight: normal
+      background: #fff9
+
+    &.operation:active
+      background: #fff5
 </style>
 
