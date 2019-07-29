328df9678aa61e8933ff2703351ae808ee003fab
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-29 12:21:56 -0300

    refactored

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-
+    <Calculator />
   </div>
 </template>
 
 <script>
+import Calculator from './components/Calculator.vue';
+
 export default {
-  
+  components: {
+    Calculator,
+  },
 };
 </script>
 
 <style lang="sass" scoped>
   #app
+    display: flex
+    justify-content: center
+    align-items: center
     height: 100vh
     background: linear-gradient(120deg, #3FB37F, #33475B )
 </style>
