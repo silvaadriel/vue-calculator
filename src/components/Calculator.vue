46422a303382310b5eae5d8212b228832df36c32
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-28 23:27:04 -0300

    created Calculator component

---
+++ src/components/Calculator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="calculator">
+    
+  </div>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Calculator",
+};
+</script>
+
+<style lang="sass" scoped>
+  
+</style>
